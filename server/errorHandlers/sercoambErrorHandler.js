9b3cccb8867ef4f1c688cd6dfba6ac2c00ae8689
AngelRubilar <angelrubilar07@gmail.com>
2025-05-18 19:46:16 -0400

    feat: agrega validacion de datos estancadas

--- server/errorHandlers/sercoambErrorHandler.js
+++ server/errorHandlers/sercoambErrorHandler.js
@@ -1,6 +1,10 @@
 const apiErrorLogger = require('../utils/apiErrorLogger');
 
 class SercoambErrorHandler {
+    constructor() {
+        this.lastTimestamps = new Map(); // Almacena los últimos timestamps de cada estación
+    }
+
     handleError(station, response, error) {
         if (error) {
             this.handleConnectionError(station, error);
@@ -35,12 +39,26 @@ class SercoambErrorHandler {
         );
         if (todosInvalidos) {
             this.handleInvalidData(station);
+            return;
+        }
+
+        // Verificar datos estancados
+        const lastRecord = data[data.length - 1];
+        if (lastRecord?.TmStamp) {
+            this.checkStaleData(station, lastRecord.TmStamp);
         }
     }
 
     handleVictoriaData(station, data) {
         if (!data?.data?.Data) {
             this.handleEmptyResponse(station);
+            return;
+        }
+
+        // Verificar datos estancados
+        const lastRecord = data.data.Data[data.data.Data.length - 1];
+        if (lastRecord?.TmStamp) {
+            this.checkStaleData(station, lastRecord.TmStamp);
         }
     }
 
@@ -49,6 +67,20 @@ class SercoambErrorHandler {
             errorType: 'INVALID_DATA',
             message: 'Todos los datos recibidos son inválidos'
         });
+    }
+
+    checkStaleData(station, currentTimestamp) {
+        const lastTimestamp = this.lastTimestamps.get(station);
+        
+        if (lastTimestamp && lastTimestamp === currentTimestamp) {
+            apiErrorLogger.logConnectionError('Sercoamb', station, {
+                errorType: 'STALE_DATA',
+                message: `Los datos no se han actualizado. Último timestamp: ${currentTimestamp}`,
+                timestamp: currentTimestamp
+            });
+        } else {
+            this.lastTimestamps.set(station, currentTimestamp);
+        }
     }
 }
 
